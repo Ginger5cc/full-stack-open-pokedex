b1b6cd2b7f03b54cb0a26517a8fd802b5f5eb83e
Ginger <gingerginger5cc@gmail.com>
2024-08-14 11:46:39 -0400

    add navigate test

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -7,4 +7,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+  test('pokemon page can be navigated to', async({ page }) => {
+    await page.goto('http://localhost:8080/')
+    await page.getByText('charmander').click()
+    await expect(page.getByText('blaze')).toBeVisible()
+    await expect(page.getByText('solar power')).toBeVisible()
+  })
 })
