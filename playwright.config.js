21736ecf76dec4612ec087e766e300535b9ad649
Ginger <gingerginger5cc@gmail.com>
2024-08-14 11:30:24 -0400

    fix the lint styles

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -81,5 +81,5 @@ module.exports = defineConfig({
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
-});
+})
 
